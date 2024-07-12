60cf714a295c131dc61988e87664b8522492e38b
Cenk Kıran <cenk33m@gmail.com>
2024-07-12 15:10:37 +0300

    unit of work class was created

---
+++ src/YoutubeProject/Infrastructure/Youtube.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Youtube.Application.Interfaces.Repositories;
+using Youtube.Application.Interfaces.UnitOfWorks;
+using Youtube.Persistence.Context;
+using Youtube.Persistence.Repositories;
+
+namespace Youtube.Persistence.UnitOfWorks;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _appDbContext;
+    public UnitOfWork(AppDbContext dbContext)
+    {
+        _appDbContext = dbContext;
+    }
+    public async ValueTask DisposeAsync() => await  _appDbContext.DisposeAsync();
+   
+
+
+    public int Save() => _appDbContext.SaveChanges();
+   
+
+    public async Task<int> SaveAsync() => await _appDbContext.SaveChangesAsync();
+    
+
+    IReadRepository<T> IUnitOfWork.GetReadRepository<T>() => new ReadRepository<T>(_appDbContext);
+  
+
+    IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>() => new WriteRepository<T>(_appDbContext);
+    
+}
