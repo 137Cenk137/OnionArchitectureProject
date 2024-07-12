57b6ad43bc350e848aa296c40ab12913b9dc984a
Cenk Kıran <cenk33m@gmail.com>
2024-07-12 15:08:05 +0300

    unit of work structure interface was fulfilled

---
+++ src/YoutubeProject/Core/Youtube.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+using Youtube.Application.Interfaces.Repositories;
+using Youtube.Domain.Common;
+
+namespace Youtube.Application.Interfaces.UnitOfWorks;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    IReadRepository<T> GetReadRepository<T>() where T : class,IEntityBase,new();
+    IWriteRepository<T> GetWriteRepository<T>() where T: class,IEntityBase,new();
+    Task<int> SaveAsync();
+    int Save();
+}
